ddb37cb58356a9bd669ac8580f159a5a76465c5d
dlmcauslan <dlmcauslan@gmail.com>
2016-05-04 15:42:21 -0700

    Created a file to download super rugby results from wikipedia

---
+++ SuperRugbyDownloader.py
@@ -0,0 +1,7 @@
+'''                   SuperRugbyDownloader.py
+Downloads historical super rugby results from wikipedia.
+Created: 04/05/2016
+'''
+import numpy as np
+import pandas as pd
+from bs4 import BeautifulSoup
