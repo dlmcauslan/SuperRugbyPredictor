f78774c6b15abc07109e2b0cb16c2b0fbef893e0
dlmcauslan <dlmcauslan@gmail.com>
2016-05-05 17:16:07 -0700

    Can download years 1996-2002 succesfully

--- SuperRugbyDownloader.py
+++ SuperRugbyDownloader.py
@@ -5,7 +5,6 @@ Created: 04/05/2016
     Saves data in MySQL database
 
 TO DO:
-    Change location of database so it is saved in project folder
     Put in a loop to download data from all years
 '''
 import numpy as np
@@ -17,7 +16,7 @@ import pymysql.cursors
 import pymysql
 
 
-def downloader(year, conn):
+def downloader(year, connect):
     #Creates dataframe
     colNames =["Position", "TeamName", "GamesPlayed", "Won", "Draw", "Lost", 
             "PointsFor", "PointsAgainst", "PointsDifferential", "BonusPoints", 
@@ -25,7 +24,7 @@ def downloader(year, conn):
     rugbyData = pd.DataFrame(dict.fromkeys(colNames,[]))
 
 
-    URLPage = 'https://en.wikipedia.org/wiki/1996_Super_12_season'     
+    URLPage = 'https://en.wikipedia.org/wiki/'+str(year)+'_Super_12_season'     
     # Creates soup and dowloads data
     soup = BeautifulSoup(urllib2.urlopen(URLPage).read())
     table = soup.find('table','wikitable sortable')
@@ -33,9 +32,14 @@ def downloader(year, conn):
     for row in table('tr'):
         tempRow=[]
         for col in row.findAll("td"):
-            # The team name is in a different format so requires the if statement
+            # Dealing with cases where col.string == None
             if col.string == None:
-                tempRow.append(col.find_all('a')[1].string)
+                # The team name is in a different format so requires the if statement
+                if col.find_all('a') != []:
+                    tempRow.append(col.find_all('a')[1].string)
+                # In 2001 the Draws column is empty, so set as 0
+                else:
+                    tempRow.append(0)
             # The negative sign is encoded in a way that needs to be changed
             elif re.findall(u'[\u2212]',col.string) != []:
                 tempRow.append(-int(re.findall(u'\d{1,3}',col.string)[0]))
@@ -50,13 +54,14 @@ def downloader(year, conn):
     [yearCol.append(year) for n in xrange(len(rugbyData))]
     rugbyData["Year"] = yearCol  
     # Add to SQL database
-    addToDatabase(rugbyData, conn)
+    addToDatabase(rugbyData, connect)
     #print rugbyData
 
 
 #createTable()
-def createTable(tableName, conn):
+def createTable(tableName, connect):
     # function which creates the table (tableName) in the database db
+    conn = pymysql.connect(connect[0], connect[1], connect[2], connect[3])
     cursor = conn.cursor()        
     #Created database
     sql_command = """
@@ -71,8 +76,9 @@ def createTable(tableName, conn):
 
         
 #removeTable()
-def removeTable(tableName, conn):
+def removeTable(tableName, connect):
     # function which removes the table (tableName) from the database db
+    conn = pymysql.connect(connect[0], connect[1], connect[2], connect[3])
     cursor = conn.cursor()        
     #Remove database
     sql_command = """ DROP TABLE IF EXISTS seasonResults """ 
@@ -83,30 +89,38 @@ def removeTable(tableName, conn):
 
                 
 # addToDatabase()
-def addToDatabase(dataFrame, conn):
+def addToDatabase(dataFrame, connect):
     # function which adds scraped data to database db
+    conn = pymysql.connect(connect[0], connect[1], connect[2], connect[3])
     dataFrame.to_sql(name = 'seasonResults', con = conn, flavor ='mysql', if_exists = 'append', index=False)       
     conn.commit()
     conn.close()
 
-
+def downloadAll(connect):
+    years = range(2003,2004,1)
+    for year in years:
+        print year
+        downloader(year, connect)
 
 host = "localhost"; user = "dlmsql"; passwd = "DLMPa$$word"; db = "superRugbyPredictor"
-conn = pymysql.connect(host, user, passwd, db)
+connect = [host, user, passwd, db]
 
 #Set to True to remove the table
-if 0:
-    removeTable('seasonResults', conn)
+if 1:
+    removeTable('seasonResults', connect)
 #Set to True to create the table
-if 0:
-    createTable('seasonResults', conn)
+if 1:
+    createTable('seasonResults', connect)
 #Set to true to download data                                                 
-if 0:                                                                
-    downloader(1996, conn)
+if 1:                                                                
+    #downloader(1996, connect)
+    downloadAll(connect)
+
 
 
 
 # Temporary code to read from database
+conn = pymysql.connect(connect[0], connect[1], connect[2], connect[3])
 sqlQuery = '''SELECT * FROM seasonResults'''                    
 dataFrame = pd.read_sql(sqlQuery, conn)
 conn.close()
