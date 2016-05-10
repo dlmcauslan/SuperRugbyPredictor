a00a30d34c0952fcf44213b5abe890576dce583e
dlmcauslan <dlmcauslan@gmail.com>
2016-05-09 18:56:54 -0700

    Now downloads data from all years 1996-2016
    Had to create a separate loop for 2016 because the data is stored in 2
    separate tables due to the conference system.
    I had to add lots of exceptions from 2011 onwards as the wikipedia
    tables had different rows these years. I left these out for now, so all
    data has the same columns as 1996.

--- SuperRugbyDownloader.py
+++ SuperRugbyDownloader.py
@@ -3,9 +3,12 @@ Downloads historical super rugby results from wikipedia.
 Created: 04/05/2016
     Implemented downloading of 1996 data
     Saves data in MySQL database
+Modified: 09/05/2016
+    Downloads data from all years. Had to add many exception because different
+    years have the data formatted slightly differently
 
 TO DO:
-    Put in a loop to download data from all years
+    Clean team name data
 '''
 import numpy as np
 import pandas as pd
@@ -22,36 +25,116 @@ def downloader(year, connect):
             "PointsFor", "PointsAgainst", "PointsDifferential", "BonusPoints", 
             "Points"]
     rugbyData = pd.DataFrame(dict.fromkeys(colNames,[]))
-
-
-    URLPage = 'https://en.wikipedia.org/wiki/'+str(year)+'_Super_12_season'     
+    
+    # Gets different URLs because the competition changed its name over the years.
+    if year <= 2005:
+        URLPage = 'https://en.wikipedia.org/wiki/{}_Super_12_season'.format(year)
+    elif year >= 2006 and year<=2010:
+        URLPage = 'https://en.wikipedia.org/wiki/{}_Super_14_season'.format(year)
+    elif year >= 2011:
+        URLPage = 'https://en.wikipedia.org/wiki/{}_Super_Rugby_season'.format(year)
     # Creates soup and dowloads data
-    soup = BeautifulSoup(urllib2.urlopen(URLPage).read())
+    soup = BeautifulSoup(urllib2.urlopen(URLPage).read(),"lxml")
     # The table layout is different depending on the year
-    if year<=2002:
+    if year<=2002 or (year>=2006 and year <= 2010):
         table = soup.find('table','wikitable sortable')
-    elif year>=2003:
+    elif year>=2003 and year<=2005:
         table = soup.find_all('table','wikitable')[1]
-    # Iterates over row and column of the table
-    for row in table('tr'):
-        tempRow=[]
-        for col in row.findAll("td"):
-            # Dealing with cases where col.string == None
-            if col.string == None:
-                # The team name is in a different format so requires the if statement
-                if col.find_all('a') != []:
-                    tempRow.append(col.find_all('a')[1].string)
-                # In 2001 the Draws column is empty, so set as 0
+    elif year>=2011 and year<=2013:
+        table = soup.find_all('table','wikitable')[3]
+    elif year>=2014 and year<=2015:
+        table = soup.find_all('table','wikitable')[0]
+    elif year>=2016:
+        table = soup.find_all('table','wikitable')[1:3]
+    rowCount = 0
+        
+    # Iterates over row and column of the table - In 2016 there are two seperate tables so need an extra loop
+    if year >= 2016:
+        for tab in table:
+            for row in tab('tr'):
+                tempRow=[]      
+                # Don't read in title row for years later than 2011
+                #if (rowCount in (range(0,5) + range(6,26,2) + range(25,30) + range(31,47,2))):
+                if (rowCount in range(0,2)):
+                    pass
                 else:
-                    tempRow.append(0)
-            # The negative sign is encoded in a way that needs to be changed
-            elif re.findall(u'[\u2212]',col.string) != []:
-                tempRow.append(-int(re.findall(u'\d{1,3}',col.string)[0]))
+                    colno = 0
+                    for col in row.findAll("td"):
+                        # Dealing with columns years later than 2011
+                        if year >=2014 and colno in [9,10]:
+                            pass
+                        elif year >=2014 and colno == 11:
+                            tmp = int(col.string)
+                        elif year >=2014 and colno == 12:
+                            tempRow.append(int(col.string)+tmp)               
+                        # Dealing with cases where col.string == None
+                        elif col.string == None:
+                            # The team name is in a different format so requires the if statement
+                            if col.find_all('a') != []:
+                                tempRow.append(col.find_all('a')[1].string)
+                        # The negative sign is encoded in a way that needs to be changed
+                        elif re.findall(u'[\u2212]',col.string) != []:
+                            tempRow.append(-int(re.findall(u'\d{1,3}',col.string)[0]))
+                        else:
+                            tempRow.append(int(col.string))
+                        # Increment colno
+                        colno+=1
+                # Don't include the first, empty row. Add the rest into a dataframe.
+                if tempRow !=[]:
+                    rugbyData = rugbyData.append(pd.DataFrame([tempRow], columns=colNames), ignore_index=True)
+                rowCount+=1
+    else:
+        for row in table('tr'):
+            tempRow=[]  
+            # Don't read in title row for years later than 2011
+            if year >= 2011 and rowCount == 0:
+                pass
+            elif year >= 2014 and (rowCount in range(1,4) or rowCount>=20):
+                pass
             else:
-                tempRow.append(int(col.string))
-        # Don't include the first, empty row. Add the rest into a dataframe.
-        if tempRow !=[]:
-            rugbyData = rugbyData.append(pd.DataFrame([tempRow], columns=colNames), ignore_index=True)
+                colno = 0
+                for col in row.findAll("td"):
+                    # Dealing with columns years later than 2011
+                    # Don't need number of byes data
+                    if year>=2011 and year <= 2013 and colno == 6:
+                        pass
+                    # Sum the different types of bonus point together
+                    elif year==2011 and colno == 10:
+                        tmp = int(col.string)
+                    elif year==2011 and colno == 11:
+                        tempRow.append(int(col.string)+tmp)
+                    # In 2012, 2 more unnecesary columns were added, so remove these
+                    elif year in [2012,2013] and colno in [10,11]:
+                        pass
+                    elif year in [2012,2013] and colno == 12:
+                        tmp = int(col.string)
+                    elif year in [2012,2013] and colno == 13:
+                        tempRow.append(int(col.string)+tmp)
+                    elif year >=2014 and colno in [9,10]:
+                        pass
+                    elif year >=2014 and colno == 11:
+                        tmp = int(col.string)
+                    elif year >=2014 and colno == 12:
+                        tempRow.append(int(col.string)+tmp)               
+                    # Dealing with cases where col.string == None
+                    elif col.string == None:
+                        # The team name is in a different format so requires the if statement
+                        if col.find_all('a') != []:
+                            tempRow.append(col.find_all('a')[1].string)
+                        # In 2001 the Draws column is empty, so set as 0
+                        else:
+                            tempRow.append(0)
+                    # The negative sign is encoded in a way that needs to be changed
+                    elif re.findall(u'[\u2212]',col.string) != []:
+                        tempRow.append(-int(re.findall(u'\d{1,3}',col.string)[0]))
+                    else:
+                        tempRow.append(int(col.string))
+                    # Increment colno
+                    colno+=1
+            # Don't include the first, empty row. Add the rest into a dataframe.
+            if tempRow !=[]:
+                rugbyData = rugbyData.append(pd.DataFrame([tempRow], columns=colNames), ignore_index=True)
+            rowCount+=1
     
     # Append a year column
     yearCol=[]
@@ -101,7 +184,7 @@ def addToDatabase(dataFrame, connect):
     conn.close()
 
 def downloadAll(connect):
-    years = range(1996,2006,1)
+    years = range(1996,2017,1)
     for year in years:
         print year
         downloader(year, connect)
@@ -125,7 +208,8 @@ if 0:
 
 # Temporary code to read from database
 conn = pymysql.connect(connect[0], connect[1], connect[2], connect[3])
-sqlQuery = '''SELECT * FROM seasonResults'''                    
+sqlQuery = '''SELECT * FROM seasonResults'''
+#sqlQuery = '''SELECT * FROM seasonResults WHERE Year = 1996'''                    
 dataFrame = pd.read_sql(sqlQuery, conn)
 conn.close()
 dataFrame 
